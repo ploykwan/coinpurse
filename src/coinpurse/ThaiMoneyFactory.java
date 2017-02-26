f9ed53b4a10f1786623c728c9ad16df042ad1dc9
Kwankaew Uttama <kwankaew.u@ku.th>
2017-02-26 20:39:42 +0700

    edit for lab4

--- src/coinpurse/ThaiMoneyFactory.java
+++ src/coinpurse/ThaiMoneyFactory.java
@@ -2,19 +2,17 @@ package coinpurse;
 
 public class ThaiMoneyFactory extends MoneyFactory {
 	private Valuable money;
-	protected static long nextSerialNumber = 1000000;
-	protected static long serialNumber;
+
 	@Override
 	public Valuable createMoney(double value) {
 		if (value == 20 || value == 50 || value == 100 || value == 500
 				|| value == 1000) {
 			money = new BankNote(value, "Bath");
-			this.serialNumber = this.nextSerialNumber;
-			this.nextSerialNumber++;
+
 		} else if (value == 1 || value == 2 || value == 5 || value == 10) {
 			money = new Coin(value, "Bath");
 		} else {
-				throw new IllegalArgumentException();
+			throw new IllegalArgumentException();
 		}
 		return money;
 	}
